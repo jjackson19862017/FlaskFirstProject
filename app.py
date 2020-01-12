06549c37aaa23045c8857b950e591c663158a5db
Stephen Jackson <jjackson19862017@gmail.com>
2020-01-12 15:24:16 +0000

    Completed Flash Routing

--- app.py
+++ app.py
@@ -6,9 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-    
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
-            port=os.environ.get("PORT"), 
+            port=int(os.environ.get("PORT")), 
             debug=True)
     
